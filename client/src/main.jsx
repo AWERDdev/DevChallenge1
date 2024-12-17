e9e2cf8a01f316a28b152bda81289aadda59c8c7
AWERDdev <kokorami565@gmail.com>
2024-12-17 15:59:23 +0200

    in work

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')).render(
  <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path='/ViewQRcode' element={<ViewQR/>}/>
+      <Route path='/ViewQRcode' element={<ViewQR />}/>
     </Routes>
   </BrowserRouter>
 )
