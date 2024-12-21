d6d6ffb3f2a0716d7966020d2e0ed5003210b7de
AWERDdev <kokorami565@gmail.com>
2024-12-21 12:50:56 +0200

    done

--- client/src/API/server.js
+++ client/src/API/server.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3500;
-const cors = require('cors');
+    const express = require('express');
+    const app = express();
+    const port = process.env.PORT || 3500;
+    const cors = require('cors');
 
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({extended:true}));
 
 
-const qrWidth = '180';
-const qrCodesize= `${qrWidth}X${qrWidth}` 
+    const qrWidth = '180';
+    const qrCodesize= `${qrWidth}X${qrWidth}` 
 
 
-app.get('/requestlink',(req,res)=>{
-    const URL = req.query.value
-    const QRCodeGenURL = `https://api.qrserver.com/v1/create-qr-code/?size=${qrCodesize}&data=${URL} `;
-    res.json(QRCodeGenURL);
-    // console.log(URL)
-});
+    app.get('/requestlink',(req,res)=>{
+        const URL = req.query.value
+        const QRCodeGenURL = `https://api.qrserver.com/v1/create-qr-code/?size=${qrCodesize}&data=${URL} `;
+        res.json(QRCodeGenURL);
+        // console.log(URL)
+    });
 
-app.listen(port,()=>{
-    console.log(`server is running on http://localhost:${port}`)
-})
+    app.listen(port,()=>{
+        console.log(`server is running on http://localhost:${port}`)
+    })
